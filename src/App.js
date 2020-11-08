a131aa02e31811a0a677607f3ae667842915038c
Dawid <Dawid_pacanowski@wp.pl>
2020-11-08 01:44:21 +0100

    Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react'
 import NavBar from './Navbar'
+import HomePage from './HomePage'
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <HomePage />
     </div>
   );
 }
---
+++ src/HomePage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import BackgroundPhoto from './Photos/1.jpg'
+function HomePage() {
+    return (
+        <div className="home-pageqqw">
+            <div className="home-Container">
+                <div className="home-box1">
+                    <div className="home-Title">
+                        <h3>Tessla is the best</h3>
+                        <h1>SWEETENER OF TEA</h1>
+                    </div>
+                    <div className="home-content">
+                        <p>Consectetur adipiscing elit. Curabitur luctus diam ut arcu elementum venenatis. Integer in mattis sapien, non fermentum ante. In a ligula purus.</p>
+                        <button>SHOP ONLINE </button>
+                    </div>
+                </div>
+                <img className="home-box2" src={BackgroundPhoto}></img>
+            </div>
+            <div className="home-text">
+                <h3>You are what you drink</h3>
+                <h1>
+                    TEA IS A DRINK OF HEALTH AND BEAUTY
+                </h1>
+                <div className="home-textDesciption">
+                    Looking for a nice cup of tea? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget laoreet lacus. Proin nulla nulla, iaculis vel nulla bibendum, molestie vestibulum nibh. Ut tincidunt nec ligula ac sodales. Sed ligula ante, pharetra ut bibendum commodo, posuere vitae mi. Aliquam cursus
+                </div>
+            </div>
+        </div>
+    )
+}
+export default HomePage
