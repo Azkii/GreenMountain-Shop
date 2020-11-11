79ba69547b390fb94e4dc7b4bcdf4f2f17215352
Dawid <Dawid_pacanowski@wp.pl>
2020-11-11 09:16:08 +0100

    Working hover on Products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Product.css'
 function Product(props) {
+    const [click, setClick] = useState(true)
+    const handleClick = () => setClick(!click)
     return (
-        <div className="product-Container">
-            <img className="product-photo" src={props.product.image} />
+        <div onMouseEnter={handleClick} onMouseLeave = {handleClick} className={click ? "product-Container" : "product-ContainerHover product-Container" }>
+            <img className="product-photo" src={props.product.image} alt="Tea-example"/>
             <h2 className="product-name">{props.product.name} Tea</h2>
             <p className="product-price">{props.product.price[0]}$~{props.product.price[2]}$</p>
+            {click ? "" : ( 
+            <div className="HoverDetails">
+                <button className="hoverDetails-Discover">
+                    Discover
+                </button>
+                <button className="hoverDetails-Checkout">
+                    ADD TO CARD
+                </button>
+            </div>)}
         </div>
     )
 }
--- src/productDataBase.js
+++ src/productDataBase.js
@@ -7,56 +7,56 @@ const ProductsDataBase = [
         sizes: ["big", "medium", "small"]
     },
     {
-        id: "1",
+        id: "2",
         name: "Oolong",
         price: [10, 15, 20],
         image: "https://moyamatcha.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2016/03/Zielona-herbata-Moya-Matcha-Tradycyjna-30g-1-510x510.jpg.webp",
         sizes: ["big", "medium", "small"]
     },
     {
-        id: "1",
+        id: "3",
         name: "Oolong",
         price: [10, 15, 20],
         image: "https://moyamatcha.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2016/03/Zielona-herbata-Moya-Matcha-Tradycyjna-30g-1-510x510.jpg.webp",
         sizes: ["big", "medium", "small"]
     },
     {
-        id: "1",
+        id: "4",
         name: "Oolong",
         price: [10, 15, 20],
         image: "https://moyamatcha.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2016/03/Zielona-herbata-Moya-Matcha-Tradycyjna-30g-1-510x510.jpg.webp",
         sizes: ["big", "medium", "small"]
     },
     {
-        id: "1",
+        id: "5",
         name: "Oolong",
         price: [10, 15, 20],
         image: "https://moyamatcha.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2016/03/Zielona-herbata-Moya-Matcha-Tradycyjna-30g-1-510x510.jpg.webp",
         sizes: ["big", "medium", "small"]
     },
     {
-        id: "1",
+        id: "6",
         name: "Oolong",
         price: [10, 15, 20],
         image: "https://moyamatcha.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2016/03/Zielona-herbata-Moya-Matcha-Tradycyjna-30g-1-510x510.jpg.webp",
         sizes: ["big", "medium", "small"]
     },
     {
-        id: "1",
+        id: "7",
         name: "Oolong",
         price: [10, 15, 20],
         image: "https://moyamatcha.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2016/03/Zielona-herbata-Moya-Matcha-Tradycyjna-30g-1-510x510.jpg.webp",
         sizes: ["big", "medium", "small"]
     },
     {
-        id: "1",
+        id: "8",
         name: "Oolong",
         price: [10, 15, 20],
         image: "https://moyamatcha.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2016/03/Zielona-herbata-Moya-Matcha-Tradycyjna-30g-1-510x510.jpg.webp",
         sizes: ["big", "medium", "small"]
     },
     {
-        id: "1",
+        id: "9",
         name: "Oolong",
         price: [10, 15, 20],
         image: "https://moyamatcha.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2016/03/Zielona-herbata-Moya-Matcha-Tradycyjna-30g-1-510x510.jpg.webp",
