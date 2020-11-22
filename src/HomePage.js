6172b18abc1509a57dcb1fc4be52d2ef69305875
Dawid <Dawid_pacanowski@wp.pl>
2020-11-23 00:46:51 +0100

    working at Home page design

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,28 +1,13 @@
 import React from 'react'
 function HomePage() {
     return (
-        <div className="home-pageqqw">
-            <div className="home-Container">
-                <div className="home-box1">
-                    <div className="home-Title">
-                        <h3>Tessla is the best</h3>
-                        <h1>SWEETENER OF TEA</h1>
-                    </div>
-                    <div className="home-content">
-                        <p>Consectetur adipiscing elit. Curabitur luctus diam ut arcu elementum venenatis. Integer in mattis sapien, non fermentum ante. In a ligula purus.</p>
-                        <button>SHOP ONLINE. </button>
-                    </div>
-                </div>
-                <div className="home-box2"> </div>
+        <div className="homePage">
+            <div className="homePage-Text">
+                <h1>Try Out our new Bubble tea</h1>
+                <p>Donec dolor metus, suscipit sed placerat vel, fermentum vitae felis. Donec dolor metus, suscipit sed placerat vel, fermentum vitae felis </p>
+                <button>ORDER HERE.</button>
             </div>
-            <div className="home-text">
-                <h3>You are what you drink</h3>
-                <h1>
-                    TEA IS A DRINK OF HEALTH AND BEAUTY
-                </h1>
-                <div className="home-textDesciption">
-                    Looking for a nice cup of tea? Lorem ipsum dolor sit amet, consectetu
-                </div>
+            <div className="homePage-PhotoSection">
             </div>
         </div>
     )
