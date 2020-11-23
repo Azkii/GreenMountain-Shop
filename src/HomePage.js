35f840af3cce0b5e283ceb69f83e75f12772eea4
Dawid <Dawid_pacanowski@wp.pl>
2020-11-23 17:44:27 +0100

    Home page completed

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,12 +2,12 @@ import React from 'react'
 function HomePage() {
     return (
         <div className="homePage">
-            <div className="homePage-Text">
-                <h1>Try Out our new Bubble tea</h1>
-                <p>Donec dolor metus, suscipit sed placerat vel, fermentum vitae felis. Donec dolor metus, suscipit sed placerat vel, fermentum vitae felis </p>
-                <button>ORDER HERE.</button>
-            </div>
             <div className="homePage-PhotoSection">
+                <div className="homePage-Text">
+                    <h1>Try Out our new Bubble tea</h1>
+                    <p>Donec dolor metus, suscipit sed placerat vel, fermentum vitae felis. Donec dolor metus, suscipit sed placerat vel, fermentum vitae felis </p>
+                    <button>ORDER HERE.</button>
+                </div>
             </div>
         </div>
     )
