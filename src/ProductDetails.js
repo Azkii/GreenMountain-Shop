86885928017ef804b3096bf262b9715a3d1660b2
Dawid <Dawid_pacanowski@wp.pl>
2020-12-01 00:30:01 +0100

    change size in progress

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,10 +1,36 @@
 import {Link} from 'react-router-dom'
 import iconArrow from './Photos/icons/arrow.svg'
+import React, {useState} from 'react'
 
 function ProductDetails(props) {
+    const [count, setCount] = useState(0);
+    let small = true
+    let medium = false
+    let big = false
+    function test() {
+        if (count === 0) {
+            small = true
+            medium = false
+            big = false
+        }
+        if (count === 1) {
+            medium = true
+            small = false
+            big = false
+        }
+        if (count === 2) {
+            big = true
+            medium = false
+            small = false
+        }
+        else {
+            small = true
+        }
+    }
+/*Import*/
   let content = <div className="productDetails-MainContainer">
                 <Link to="/">
-                <img className="productDetails-BackButton" src={iconArrow} />
+                <img className="productDetails-BackButton" src={iconArrow} alt="arrowSign" />
                 </Link>
                 <div className="productDetails-Box">
                     <div className="productDetails-Photos">
@@ -33,13 +59,11 @@ function ProductDetails(props) {
                             <p className="productDetails-Avaible">Availability (In stock)</p>
                         </section>
                         <div className="productDetails-PriceSection">
-                            <p>$ {props.product.price[0]}</p>
+                            <p>$ {props.product.price[count]}</p>
                             <ul className="productDetails-Size">
-                                <li className="productDetails-SizeHover" >S</li>
-                                <li>|</li>
-                                <li className="productDetails-SizeHover">M</li>
-                                <li>|</li>
-                                <li className="productDetails-SizeHover">B</li>
+                                <li className = { small ? "productDetails-SizeHovered" : "productDetails-SizeHover"} onClick={() => {setCount(0); test()}} >S</li>
+                                <li className = { medium ? "productDetails-SizeHovered" : "productDetails-SizeHover"} onClick={() => {setCount(1); test()}} >M</li>
+                                <li className = { big ? "productDetails-SizeHovered" : "productDetails-SizeHover"} onClick={() => {setCount(2); test()}} >B</li>
                             </ul>
                         </div>
                         <section>
@@ -47,7 +71,10 @@ function ProductDetails(props) {
                                 <li>Product Info.</li>
                             </ul>
                             <div className="productDetails-Info">
-                                Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumem ipsumLorem ipsumLorem ipsumem ipsumLorem ipsumLorem ipsumem ipsumLorem ipsumLorem ipsumem ipsumLorem ipsumLorem ipsum
+                                Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem 
+                                ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem 
+                                ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumem ipsumLorem ipsumLorem ipsumem 
+                                ipsumLorem ipsumLorem ipsumem ipsumLorem ipsumLorem ipsumem ipsumLorem ipsumLorem ipsum
                             </div>
                         </section>
                         <div className="productDetails-Buttons">
@@ -64,6 +91,7 @@ function ProductDetails(props) {
                     </div>
                 </div>
                 </div>
+    /*END of import*/
     if ( "/product/" + props.product.name + props.product.id !== decodeURI(window.location.pathname) ){
         content = ""
    }
