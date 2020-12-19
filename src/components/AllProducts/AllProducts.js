63b71188f4954e17906bb9c0919ed68541717592
Dawid <Dawid_pacanowski@wp.pl>
2020-12-19 16:30:07 +0100

    allProducts

--- src/components/AllProducts/AllProducts.js
+++ src/components/AllProducts/AllProducts.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import TeaFinder from '../TeaFinder/TeaFinder'
 import './AllProducts.css'
 import CategoriesAll from './CategoriesAll'
 import ProductsDataBase from '../../productDataBase'
 import Product from './AllProducts-Product'
-import ProductMobile from './AllProducts-ProductMobile'
+import ProductMobile from '../Product/ProductMobile'
 class AllProducts extends React.Component {
     constructor(props) {
         super(props)
